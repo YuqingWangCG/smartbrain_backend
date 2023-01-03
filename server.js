5b3a5e9f6c216e7661aa42ca47321e08c449929b
YuqingWangCG <yuqing_wang@protonmail.com>
2023-01-03 15:59:31 +0100

    fix port

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)})
 
  
 app.listen(process.env.PORT || 3002, ()=>{
-	console.log(`app is running on port ${process.env.DATABASE_PORT}`);
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
